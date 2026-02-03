77b1ef18ad4d0a40c7517f285d9dfed44eb80d1b
adelicioribeirosobral980-cmd <adelicioribeirosobral980@gmail.com>
2026-02-03 18:07:32 -0300

    Add files via upload
    Upload meu site

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("btnMensagem").addEventListener("click", function () {
+  alert("Valorizar a diversidade cultural é respeitar a história e o futuro do Norte de Minas Gerais!");
+});
